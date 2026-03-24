72b15706d020e7435270348ca7d8dffd1a13fa8d
Zakariasisu5 <zakariasisu5@gmail.com>
2026-03-23 18:45:17 -0700

    feat(routes): register transaction search route

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -3,6 +3,7 @@ const {
   addTransaction,
   getPendingTransactions,
   getAllTransactions,
+  searchTransactions, // TASK 1: New search endpoint
 } = require('../controllers/transaction.controller');
 const { validateBody } = require('../middleware/validateRequest.middleware');
 const { writeLimiter } = require('../middleware/rateLimit.middleware');
@@ -12,5 +13,6 @@ const router = Router();
 router.post('/', writeLimiter, validateBody(['fromAddress', 'toAddress', 'amount']), addTransaction);
 router.get('/pending', getPendingTransactions);
 router.get('/all', getAllTransactions);
+router.get('/search', searchTransactions); // TASK 1: Search & filter transactions
 
 module.exports = router;
